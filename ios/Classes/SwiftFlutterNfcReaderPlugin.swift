85224982ed4c32b5ac6b16286f7f3bdac5f5eff4
Igor Czapski <igor.czapski@netbulls.io>
2019-02-22 16:23:30 +0100

    Unify format of iOS results with android

--- ios/Classes/SwiftFlutterNfcReaderPlugin.swift
+++ ios/Classes/SwiftFlutterNfcReaderPlugin.swift
@@ -29,6 +29,9 @@ public class SwiftFlutterNfcReaderPlugin: NSObject, FlutterPlugin {
             activateNFC(instruction)
         case "NfcStop":
             disableNFC()
+        case "NfcWrite":
+            let data = [kId: "", kContent: nil, kError: "This method is not supported by iOS", kStatus: "error"]
+            resulter?(data)
         default:
             result("iOS " + UIDevice.current.systemVersion)
         }
@@ -55,7 +58,7 @@ extension SwiftFlutterNfcReaderPlugin {
     
     func disableNFC() {
         nfcSession?.invalidate()
-        let data = [kId: "", kContent: "", kError: "", kStatus: "stopped"]
+        let data = [kId: "", kContent: nil, kError: "", kStatus: "stopped"]
 
         resulter?(data)
         resulter = nil
@@ -69,18 +72,57 @@ extension SwiftFlutterNfcReaderPlugin : NFCNDEFReaderSessionDelegate {
     
     public func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
         guard let message = messages.first else { return }
-	    guard let payload = message.records.first else { return }
-	    guard let payloadContent = String(data: payload.payload, encoding: String.Encoding.utf8) else { return }
-
-        let data = [kId: "", kContent: payloadContent, kError: "", kStatus: "read"]
+        var records = message.records;
+        var payloadList:[String] = [];
+        for record in records {
+            var typeString = String(data: record.type, encoding: String.Encoding.utf8);
+            var idString = String(data: record.identifier, encoding: String.Encoding.utf8);
+            var payloadBytesLength = record.payload.count;
+            if(typeString == nil || idString == nil){ //TODO check if payload exists?
+                return ;
+            }
+            var payloadBytes = record.payload.withUnsafeBytes{ (bytes: UnsafePointer<UInt8>) in Array(UnsafeBufferPointer(start: bytes, count: record.payload.count / MemoryLayout<UInt8>.stride)) }
+            if(record.typeNameFormat == NFCTypeNameFormat.nfcWellKnown) {
+                if(typeString == "T"){
+                    var payloadToAppend = parseTextPayload(payloadBytes: payloadBytes, length: payloadBytesLength);
+                    if(payloadToAppend != nil){
+                        payloadList.append(payloadToAppend!);
+                    }
+                }
+            }
+        }
+        let data: [String:Any] = [kId: "", kContent: payloadList, kError: "", kStatus: "read"]
 
         resulter?(data)
         disableNFC()
     }
     
+    private func parseTextPayload(payloadBytes: [UInt8], length: Int) -> String? {
+        if length < 1 {
+            return nil
+        }
+        // Parse first byte Text Record Status Byte.
+        let isUTF16 = (Int(payloadBytes[0]) & 0x080) == 0
+        let codeLength: Int = Int(payloadBytes[0]) & 0x7f
+        
+        if length < Int(1 + codeLength) {
+            return nil
+        }
+        
+        // Get lang code and text.
+        let langCode = String(bytes: payloadBytes[1...codeLength], encoding: String.Encoding.utf8);
+        let textStartingPos = 1+codeLength;
+        let text = String(bytes: payloadBytes[textStartingPos...payloadBytes.count-1], encoding: String.Encoding.utf8);
+        if langCode == nil || text == nil {
+            return nil
+        }
+        //TODO make use of lang code
+        return text;
+    }
+    
     public func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
         print(error.localizedDescription)
-        let data = [kId: "", kContent: "", kError: error.localizedDescription, kStatus: "error"]
+        let data = [kId: "", kContent: nil, kError: error.localizedDescription, kStatus: "error"]
 
         resulter?(data)
         disableNFC()
